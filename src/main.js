ede968da6b92677aac4ec3691dc2b3ea995c1452
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-07-03 16:09:22 +0300

    Added initial settings, files and libraries

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import './js/api';
+// import './js/header';
+// import './js/hero';
+// import './js/books';
+// import './js/feedbacks';
+// import './js/events';
+// import './js/article';
+// import './js/location';
+// import './js/footer';
+// import './js/modal';
