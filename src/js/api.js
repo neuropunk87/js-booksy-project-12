f57f2948c442b3df6085116574b15e3a56eb7be6
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-07-04 19:41:45 +0300

    Added requests in api.js and img folder

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,25 @@
-import axios from axios;
+import axios from 'axios';
+
+const API_BASE = 'https://books-backend.p.goit.global/books';
+
+export async function fetchCategories() {
+  const res = await axios.get(`${API_BASE}/category-list`);
+  return res.data;
+}
+
+export async function fetchBooksByCategory(category) {
+  const res = await axios.get(`${API_BASE}/category`, {
+    params: { category },
+  });
+  return res.data;
+}
+
+export async function fetchTopBooks() {
+  const res = await axios.get(`${API_BASE}/top-books`);
+  return res.data.flatMap(item => item.books);
+}
+
+export async function fetchBookById(id) {
+  const res = await axios.get(`${API_BASE}/${id}`);
+  return res.data;
+}
