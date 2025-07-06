4d26b3671dc4a3888afc9b32e5cc96b6cd39d298
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-07-06 23:09:04 +0300

    added another way to inform user of its certain book amount in its cart

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,7 +9,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 const bookModal = document.querySelector('.book-modal-overlay');
 const bookModalClose = document.querySelector('.book-modal-close');
 // Можлива ініціалізація інформації про вміст товару в корзині
-// const productAmount = document.querySelector('.prod-count');
+const productAmount = document.querySelector('.prod-count');
 const counterAdd = document.querySelector('#btn-add');
 const counterReduce = document.querySelector('#btn-reduce');
 const counter = document.querySelector('.counter');
@@ -45,6 +45,12 @@ window.openBookModal = function (book) {
   bookModal.classList.add('open');
   document.body.style.overflow = 'hidden';
   bookModalClose && bookModalClose.focus();
+  const itemCount = localStorage.getItem(bookName.textContent) || 0;
+  // Перевірка на наявність продукту у корзині
+  if (!itemCount) {
+    productAmount.innerHTML = ''; 
+  } else
+  {productAmount.innerHTML = `Загальна кількість товару в корзині: ${localStorage.getItem(bookName.textContent)}`}
 };
 
 // Додавання та віднімання кількості товару
@@ -99,8 +105,8 @@ addToCart.addEventListener('click', () => {
   const itemCount = localStorage.getItem(bookName.textContent) || 0;
   const updatedCount = parseInt(itemCount) + modalCount;
   localStorage.setItem(bookName.textContent, updatedCount);
-  // Можливий функціонал отримання інформації про вміст товару в корзині
-  // productAmount.innerHTML = `Загальна кількість товару в корзині: ${updatedCount}`;
+  // Функціонал отримання інформації про вміст товару в корзині
+  productAmount.innerHTML = `Загальна кількість товару в корзині: ${updatedCount}`;
   iziToast.success({
     message: `Додано в корзину: ${modalCount} (Цього товару в корзині: ${updatedCount})`,
     position: 'bottomCenter',
