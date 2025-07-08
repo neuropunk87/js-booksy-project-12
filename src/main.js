987cfc00873861db10764f55ba66fad05b328c3d
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-07-08 19:57:12 +0300

    Added minor changes and removed unnecessary stuff

--- src/main.js
+++ src/main.js
@@ -2,14 +2,8 @@ import './js/api';
 import './js/header';
 // import './js/hero';
 import './js/books';
-// import './js/feedbacks';
+import './js/feedbacks';
 import './js/events';
-// import './js/article';
-// import './js/location';
-// import './js/footer';
+import './js/footer';
 import './js/modal';
-import './js/scrollbtn'
-
-
-
-
+import './js/scrollbtn';
