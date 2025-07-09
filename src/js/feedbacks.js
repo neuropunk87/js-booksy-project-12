841cbfdc41f0c79f17e91670eb866b4544bd4d11
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-07-09 17:48:25 +0300

    .

--- src/js/feedbacks.js
+++ src/js/feedbacks.js
@@ -1,11 +1,16 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination, Keyboard, Mousewheel } from 'swiper/modules';
 
+const pagEl = document.querySelector('.swiper-pagination');
+const prevBtn = document.querySelector('.feedback-button-prev');
+const nextBtn = document.querySelector('.feedback-button-next');
+
 const swiper = new Swiper('.swiper', {
   modules: [Navigation, Pagination, Keyboard, Mousewheel],
   slidesPerView: 1,
   spaceBetween: 16,
-
+  observer: true,
+    observeParents: true,
   breakpoints: {
     768: { 
       slidesPerView: 2,
@@ -37,34 +42,69 @@ const swiper = new Swiper('.swiper', {
   },
 
   on: {
-    slideChange: function () {
-      const prevBtn = document.querySelector('.feedback-button-prev');
-      const nextBtn = document.querySelector('.feedback-button-next');
-
-      if (this.isBeginning) {
-        prevBtn.classList.add('is-disabled');
-      } else {
-        prevBtn.classList.remove('is-disabled');
-      }
-
-      if (this.isEnd) {
-        nextBtn.classList.add('is-disabled');
-      } else {
-        nextBtn.classList.remove('is-disabled');
-      }
+    init() {
+      toggleNav(this)
     },
-
-    afterInit: function () {
-      const prevBtn = document.querySelector('.feedback-button-prev');
-      const nextBtn = document.querySelector('.feedback-button-next');
-
-      if (this.isBeginning) {
-        prevBtn.classList.add('is-disabled');
-      }
-      if (this.isEnd) {
-        nextBtn.classList.add('is-disabled');
-      }
+    resize() {
+      toggleNav(this)
     },
+    slideChange() {
+      toggleNav(this)
+    }
   },
 });
 
+function toggleNav(sw) {
+    const total = sw.slides.length;
+    const visible = Math.floor(sw.params.slidesPerView) || 1;
+    const hideAll = total <= visible;
+
+    if (prevBtn && nextBtn) {
+      if (hideAll) {
+        prevBtn.classList.add('is-hidden');
+        nextBtn.classList.add('is-hidden');
+      } else {
+        prevBtn.classList.remove('is-hidden');
+        nextBtn.classList.remove('is-hidden');
+        prevBtn.classList.toggle('swiper-button-disabled', sw.isBeginning);
+        nextBtn.classList.toggle('swiper-button-disabled', sw.isEnd);
+      }
+    }
+
+    if (pagEl) {
+      pagEl.style.display = hideAll ? 'none' : 'flex';
+    }
+  }
+
+// slideChange: function () {
+
+//       if (this.isBeginning) {
+//         prevBtn.classList.add('is-disabled');
+//       } else {
+//         prevBtn.classList.remove('is-disabled');
+//       }
+
+//       if (this.isEnd) {
+//         nextBtn.classList.add('is-disabled');
+//       } else {
+//         nextBtn.classList.remove('is-disabled');
+//       }
+//       if (pagEl) {
+//       pagEl.style.display = hideAll ? 'none' : 'flex';
+//     }
+//     },
+
+//     afterInit: function () {
+//       const prevBtn = document.querySelector('.feedback-button-prev');
+//       const nextBtn = document.querySelector('.feedback-button-next');
+
+//       if (this.isBeginning) {
+//         prevBtn.classList.add('is-disabled');
+//       }
+//       if (this.isEnd) {
+//         nextBtn.classList.add('is-disabled');
+//       }
+//       if (pagEl) {
+//       pagEl.style.display = hideAll ? 'none' : 'flex';
+//     }
+//     },
